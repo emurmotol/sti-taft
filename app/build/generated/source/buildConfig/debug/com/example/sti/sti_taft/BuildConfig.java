cda34b58278990a840fa3e9c1575167a907b496f
Klarizon Emar <klarizonemar@gmail.com>
2016-03-25 03:24:46 +0800

    Added files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/example/sti/sti_taft/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.sti.sti_taft;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.sti.sti_taft";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
